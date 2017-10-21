0c1015bbc946ba4efd92940707171d4e03a957f4
AdrianKauz <akauz@hispeed.ch>
2017-10-21 04:52:40 +0200

    Added dictionary-part for later use...

--- erstelle_einlassplan.js
+++ erstelle_einlassplan.js
@@ -1,11 +1,11 @@
 /**
- * Testscript
+ * Einlassplan-Script
  * @author Adrian Kauz
  */
 
 var strScriptFullPath = WScript.ScriptFullName;
 var strScriptPath = strScriptFullPath.substring(0, strScriptFullPath.lastIndexOf("\\"));
-var strScriptName = "Einlassplan-Script"
+var strScriptName = "Einlassplan-Script";
 var strVorlage = "einlassplan_vorlage.xlsx";
 var oArgs = WScript.Arguments;
 var oCom;
@@ -19,36 +19,38 @@ main()
 */
 function main()
 {
-	oCom = new ComObjects();
+    oCom = new ComObjects();
 
-	if(!oCom.loadAllObjects()) {
-		WScript.echo("Konnte nicht alle COM-Objekte laden!");
-		return;
-	}
+    if(!oCom.loadAllObjects()) {
+        WScript.echo("Konnte nicht alle COM-Objekte laden!");
+        return;
+    }
 
-	if(oArgs.length !== 1) {
-		showExclamationBox("No Arguments!")
-		return;
-	}
+    if(oArgs.length !== 1) {
+        showExclamationBox("No Arguments!");
+        return;
+    }
 
-	if(isCSVFile(oArgs(0)) === false) {
-		showExclamationBox("Keine oder ungueltige Datei!")
-		return;
-	}
+    if(isCSVFile(oArgs(0)) === false) {
+        showExclamationBox("Keine oder ungueltige Datei!");
+        return;
+    }
 
-	//var arrAllMovies = loadAllMovies(oArgs(0));
+    //var arrScreenings = loadScreeningsFromCSV(oArgs(0));
 
 
 
 
 /*
-	for(var x = 0; x < arrAllMovies.length; x++){
+    for(var x = 0; x < arrAllMovies.length; x++){
         showInfoBox(arrAllMovies[x].toString());
-	}
+    }
 */
-	oCom.Excel.Visible = true;
-	oWorkbook = oCom.Excel.Workbooks.Open(strScriptPath + "\\" + strVorlage);
-	oWorkSheet = oWorkbook.ActiveSheet;
+    oCom.Excel.Visible = true;
+    //oWorkbook = oCom.Excel.Workbooks.Open(strScriptPath + "\\" + strVorlage);
+	oWorkbook = oCom.Excel.Workbooks.Open(oArgs(0));
+
+    oWorkSheet = oWorkbook.ActiveSheet;
 
 }
 
@@ -60,20 +62,20 @@ ComObjects()
 */
 function ComObjects()
 {
-	this.Shell = null;
-	this.FSO = null;
-	this.Excel = null;
+    this.Shell = null;
+    this.FSO = null;
+    this.Excel = null;
 
-	this.loadAllObjects = function() {
-		try{
-			this.Shell = new ActiveXObject("WScript.Shell");
-			this.FSO = new ActiveXObject("Scripting.FileSystemObject");
-			this.Excel = new ActiveXObject("Excel.Application");
-			return true;
-		} catch(ex) {
-			return false;
-		}
-	}
+    this.loadAllObjects = function() {
+        try{
+            this.Shell = new ActiveXObject("WScript.Shell");
+            this.FSO = new ActiveXObject("Scripting.FileSystemObject");
+            this.Excel = new ActiveXObject("Excel.Application");
+            return true;
+        } catch(ex) {
+            return false;
+        }
+    };
 }
 
  /*
@@ -83,7 +85,7 @@ showExclamationBox()
 */
 function showExclamationBox(sMessage)
 {
-	oCom.Shell.popup(sMessage, 0, sScriptName, 48 );
+    oCom.Shell.popup(sMessage, 0, sScriptName, 48 );
 }
 
 /*
@@ -103,54 +105,54 @@ isCSVFile()
 */
 function isCSVFile(sPath)
 {
-	if(oCom.FSO.fileExists(oArgs(0))){
-		if(sPath.toLowerCase().indexOf(".csv") !== -1) {
-			return true;
-		}
-	}
+    if(oCom.FSO.fileExists(oArgs(0))){
+        if(sPath.toLowerCase().indexOf(".csv") !== -1) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
  /*
 ================
-parseCSV()
+loadScreeningsFromCSV()
 ================
 */
-function loadAllMovies(sPath)
+function loadScreeningsFromCSV(sPath)
 {
     var regex = /(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](20)\d\d/g;
-	var oFile = oCom.FSO.OpenTextFile(sPath, 1);
-	var arrMovieObjects = [];
+    var oFile = oCom.FSO.OpenTextFile(sPath, 1);
+    var arrMovieObjects = [];
 
-	while(!oFile.AtEndOfStream) {
-		var arrCurrLine = oFile.ReadLine().split(",");
+    while(!oFile.AtEndOfStream) {
+        var arrCurrLine = oFile.ReadLine().split(",");
 
-		if(arrCurrLine[0].match(regex) !== null){
-			var movieObject = new MovieObject();
+        if(arrCurrLine[0].match(regex) !== null){
+            var movieObject = new MovieObject();
 
-			movieObject.Datum				= arrCurrLine[0];
-            movieObject.SaalName			= arrCurrLine[1];
-            movieObject.SaalNummer			= arrCurrLine[2];
-            movieObject.Schiene				= arrCurrLine[3];
-            movieObject.VorstellungStart	= arrCurrLine[4];
-            movieObject.HauptfilmStart		= arrCurrLine[5];
-            movieObject.Pause				= arrCurrLine[6];
-            movieObject.PauseEnde			= arrCurrLine[7];
-            movieObject.Dialicht			= arrCurrLine[8];
-            movieObject.Ende				= arrCurrLine[9];
-            movieObject.Format				= arrCurrLine[10];
-            movieObject.Filmtitel			= arrCurrLine[11];
-            movieObject.FSK					= arrCurrLine[12];
-            movieObject.Aufraumzeit			= arrCurrLine[13];
-            movieObject.NV					= arrCurrLine[14];
-            movieObject.NVFormat			= arrCurrLine[15];
-            movieObject.NVFilmTitel			= arrCurrLine[16];
+            movieObject.Datum                   = arrCurrLine[0];   // "Datum"
+            movieObject.SaalName                = arrCurrLine[1];   // "Saal"
+            movieObject.SaalNummer              = arrCurrLine[2];   // "Saal"
+            movieObject.Schiene                 = arrCurrLine[3];   // "Schiene"
+            movieObject.VorstellungStart        = arrCurrLine[4];   // "Vorstellung Start"
+            movieObject.HauptfilmStart          = arrCurrLine[5];   // "Hauptfilm Start"
+            movieObject.Pause                   = arrCurrLine[6];   // "Pause"
+            movieObject.PauseEnde               = arrCurrLine[7];   // "Pause-Ende"
+            movieObject.Dialicht                = arrCurrLine[8];   // "Dialicht"
+            movieObject.Ende                    = arrCurrLine[9];   // "Ende"
+            movieObject.Format                  = arrCurrLine[10];  // "2D/3D"
+            movieObject.Filmtitel               = arrCurrLine[11];  // "Filmtitel"
+            movieObject.FSK                     = arrCurrLine[12];  // "FSK"
+            movieObject.Aufraumzeit             = arrCurrLine[13];  // "Aufräumzeit"
+            movieObject.NV                      = arrCurrLine[14];  // "Nächste Vorstellung"
+            movieObject.NVFormat                = arrCurrLine[15];  // "2D/3D"
+            movieObject.NVFilmTitel             = arrCurrLine[16];  // "Filmtitel"
             arrMovieObjects.push(movieObject);
-		}
-	}
+        }
+    }
 
-	return arrMovieObjects;
+    return arrMovieObjects;
 }
 
 /*
@@ -179,8 +181,8 @@ function MovieObject()
     this.NVFilmTitel = null;
 
     this.toString = function(){
-    	return "Datum:\t\t" + this.Datum
-			+ "\nSaalName:\t" + this.SaalName
+        return "Datum:\t\t" + this.Datum
+            + "\nSaalName:\t" + this.SaalName
             + "\nSaalNummer:\t" + this.SaalNummer
             + "\nSchiene:\t\t" + this.Schiene
             + "\nVorstellungStart:\t" + this.VorstellungStart
@@ -196,5 +198,92 @@ function MovieObject()
             + "\nNV:\t\t" + this.NV
             + "\nNVFormat:\t" + this.NVFormat
             + "\nNVFilmTitel:\t" + this.NVFilmTitel;
-	}
+    }
+}
+
+/*
+================
+Dictionary()
+================
+*/
+function Dictionary()
+{
+    this.arrDictionary = null;
+
+    if (this.arrDictionary === null) {
+        this.arrDictionary = [];
+    }
+
+	/**
+     * Add new element to the dictionary.
+	 * @param {String} sKey
+     * @param {Object} oObject
+     * @returns {boolean}
+	 */
+    this.Add = function(sKey, oObject)
+    {
+        if (sKey !== "" && sKey.constructor === String) {
+	        for (var x = 0; x < this.arrDictionary.length; x++) {
+		        if (this.arrDictionary[x]._sKey === sKey) {
+			        this.arrDictionary[x]._oObject = oObject;
+			        return true;
+		        }
+	        }
+
+	        this.arrDictionary.push({_sKey : sKey, _oObject : oObject});
+	        return true;
+        }
+
+        return false;
+    };
+
+
+	/**
+	 * If exists, return object from dictionary element.
+	 * @param {String} sKey
+     * @returns {Object}
+	 */
+	this.Get = function(sKey)
+	{
+		if (sKey.constructor === String) {
+			for (var x = 0; x < this.arrDictionary.length; x++) {
+				if(this.arrDictionary[x]._sKey === sKey) {
+					return this.arrDictionary[x]._oObject;
+                }
+            }
+        }
+
+		return null;
+	};
+
+
+	/**
+	 * Checks if dictionary entry already exists
+	 * @param {String} sKey
+     * @returns {object}
+	 */
+	this.ContainsKey = function(sKey)
+    {
+	    if (sKey.constructor === String) {
+		    for (var x = 0; x < this.arrDictionary.length; x++) {
+			    if(this.arrDictionary[x]._sKey === sKey) {
+				    return true;
+			    }
+		    }
+
+		    return false;
+	    }
+
+	    return null;
+    };
+
+
+	/**
+	 * Returns size of dictionary
+	 * @returns {number}
+	 */
+	this.Count = function()
+    {
+        return this.arrDictionary.length;
+    };
 }
